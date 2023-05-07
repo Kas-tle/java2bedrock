41ace33ab947ed12858312bd4d63b0d3794a17b6
Joshua Castle <26531652+Kas-tle@users.noreply.github.com>
2023-05-07 00:28:29 -0700

    Init texture mapping (curr @ entity/cat)
    Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

---
+++ src/converter/textures.ts
@@ -0,0 +1,3 @@
+// Hierarchy is a bit of an issue here because some of this can be modified at different levels
+// Consider: texture path can be changed at the model level, but also the location of the file itself can be changed
+// At the base level, we need some sort of list
